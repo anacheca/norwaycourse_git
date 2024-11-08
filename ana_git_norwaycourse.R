a180e29a6aaee90f6ff0d852fd077f6392defa4c
anacheca <ana.martinez-checa2@nhm.ac.com>
2024-11-08 10:17:33 +0000

    adds instructions

--- ana_git_norwaycourse.R
+++ ana_git_norwaycourse.R
@@ -5,3 +5,10 @@ git_default_branch_rename()
 git_default_branch_configure(name = "main")
 
 #commit to second branch
+
+#fork
+
+library(usethis)
+create_from_github("86fyko/Git_github", fork = TRUE)
+
+#git push on terminal if it does not let me pull
